b3a4677f4f0650653e571580845feee5878fd460
zah <zahary@metacraft-labs.com>
2025-05-17 18:13:41 +0300

    feat: Devcontainer configuration for nix/direnv (#15)

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Development environment for codetracer-ruby-recorder";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forEachSystem (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ ruby just ];
+          };
+        });
+    };
+}
