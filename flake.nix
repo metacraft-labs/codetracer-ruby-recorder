2a7f0e9d38961cf80c458a7e76225a6b2b791566
zah <zahary@gmail.com>
2025-05-17 17:21:06 +0300

    feat: add nix devshell

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.ruby pkgs.just ];
+        };
+      });
+}
