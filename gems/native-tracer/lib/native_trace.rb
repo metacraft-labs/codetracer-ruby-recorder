348a911debc4f0b2ac3fc59c940cec269d3230ec
zah <zahary@gmail.com>
2025-05-20 02:52:07 +0300

    fix(native tracer): handle cross-platform extension

--- gems/native-tracer/lib/native_trace.rb
+++ gems/native-tracer/lib/native_trace.rb
@@ -3,6 +3,8 @@
 # Simple utility loading the native tracer extension and executing a program.
 
 require 'optparse'
+require 'fileutils'
+require 'rbconfig'
 
 options = {}
 parser = OptionParser.new do |opts|
@@ -27,10 +29,20 @@ ENV['CODETRACER_RUBY_RECORDER_OUT_DIR'] = out_dir
 
 # Path to the compiled native extension
 ext_dir = File.expand_path('../ext/native_tracer/target/release', __dir__)
-target_path = File.join(ext_dir, 'codetracer_ruby_recorder.so')
+dlext = RbConfig::CONFIG['DLEXT']
+target_path = File.join(ext_dir, "codetracer_ruby_recorder.#{dlext}")
 unless File.exist?(target_path)
-  alt_path = File.join(ext_dir, 'libcodetracer_ruby_recorder.so')
-  target_path = alt_path if File.exist?(alt_path)
+  extensions = %w[so bundle dylib dll]
+  alt_path = extensions
+             .map { |ext| File.join(ext_dir, "libcodetracer_ruby_recorder.#{ext}") }
+             .find { |path| File.exist?(path) }
+  if alt_path
+    begin
+      File.symlink(alt_path, target_path)
+    rescue StandardError
+      FileUtils.cp(alt_path, target_path)
+    end
+  end
 end
 
 recorder = nil
